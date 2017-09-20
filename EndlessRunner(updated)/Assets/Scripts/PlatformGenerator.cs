b95f265099bc7f353b27d6cdc6dde58fd5392e90
LungJoe <lungjoe107@gmail.com>
2017-09-20 14:39:17 -0500

    gitIgnore test change

---
+++ EndlessRunner(updated)/Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour {
+    
+    
+    public GameObject thePlatform;
+    public Transform generationPoint;
+    public float distanceBetween;
+    
+    private float platformWidth;
+    
+    public float distanceBetweenMin;
+    public float distanceBetweenMax;
+    
+    
+    //public GameObject[] thePlatforms;
+    private int platformSelector;
+    private float[] platformWidths;
+
+    public ObejctPooler[] theObjectPools;
+
+    private float minHeight;
+    public Transform maxHeightPoint;
+    private float maxHeight;
+    public float maxHeightChange;
+    private float heightChange;
+    
+    private CoinGenerator theCoinGenerator;
+    public float randomCoinThreshold;
+
+    public float randomSpikeThreshold;
+    public ObejctPooler spikePool;
+
+    public float powerupHeight;
+    public ObejctPooler powerupPool;
+    public float powerupThreshold;
+
+    //public ObejctPooler theObjectPool;
+
+
+
+	// Use this for initialization
+	void Start () {
+        //platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;
+
+
+
+        platformWidths = new float[theObjectPools.Length];
+
+        for (int i = 0; i < theObjectPools.Length; i++)
+        {
+            platformWidths[i] = theObjectPools[i].pooledObject.GetComponent<BoxCollider2D>().size.x;
+        }
+
+        minHeight = transform.position.y;
+        maxHeight = maxHeightPoint.position.y;
+        theCoinGenerator = FindObjectOfType<CoinGenerator>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < generationPoint.position.x)
+        {
+            distanceBetween = Random.Range(distanceBetweenMin, distanceBetweenMax);
+
+            platformSelector = Random.Range(0, theObjectPools.Length);
+
+            heightChange = transform.position.y + Random.Range(maxHeightChange, -maxHeightChange);
+
+            if (heightChange > maxHeight)
+            {
+                heightChange = maxHeight;
+            }
+            else if (heightChange < minHeight)
+            {
+                heightChange = minHeight;
+            }
+            if (Random.Range(0f,100f) < powerupThreshold)
+            {
+                GameObject newPowerup = powerupPool.GetPooledObject();
+                newPowerup.transform.position = transform.position + new Vector3((distanceBetween / 2f) + 4.3f, Random.Range((powerupHeight/2),powerupHeight), 0f);
+                newPowerup.SetActive(true);
+            }
+
+            transform.position = new Vector3(transform.position.x + (platformWidths[platformSelector] / 2) + distanceBetween, heightChange, transform.position.z);
+
+            //Instantiate(/*thePlatform*/ thePlatforms[platformSelector], transform.position, transform.rotation);
+
+            GameObject newPlatform = theObjectPools[platformSelector].GetPooledObject();
+
+            newPlatform.transform.position = transform.position;
+            newPlatform.transform.rotation = transform.rotation;
+            newPlatform.SetActive(true);
+
+
+            if (Random.Range(0f, 100f) < randomCoinThreshold)
+            {
+                theCoinGenerator.SpawnCoins(new Vector3(transform.position.x + 4.3f, transform.position.y + 1f, transform.position.z));
+            }
+            
+            
+            if (Random.Range(0f, 100f) < randomSpikeThreshold)
+            {
+                GameObject newSpike = spikePool.GetPooledObject();
+
+                float spikeXPosition = Random.Range((-platformWidths[platformSelector]+4.3f) / 2f + 3f, (platformWidths[platformSelector]+4.3f) / 2f + 1.7f);
+
+
+                Vector3 spikePosition = new Vector3(spikeXPosition, 0.5f, 0f);
+
+
+                newSpike.transform.position = transform.position + spikePosition;
+                newSpike.transform.rotation = transform.rotation;
+                newSpike.SetActive(true);
+            }
+            
+            transform.position = new Vector3(transform.position.x + (platformWidths[platformSelector] / 2) , transform.position.y, transform.position.z);
+
+        }
+	}
+}
