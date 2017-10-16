8c5bdd6f30fd5abb8af34359d2ca85df8b6c5a5f
Jhon <camreonkel@gmail.com>
2017-10-16 15:41:24 -0500

    Bank Implemented
    Minor bug, could use help

--- EndlessRunner(updated)/Assets/Scripts/DeathMenu.cs
+++ EndlessRunner(updated)/Assets/Scripts/DeathMenu.cs
@@ -2,11 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class DeathMenu : MonoBehaviour {
 
     public string mainMenuLevel;
+    public Text bank;
+    public Text grabbed;
+    public Text total;
+    public ScoreManager score;
 
+    public int currentStash;
+    public int collected;
+    public int totalCoins;
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("Bank"))
+        {
+            currentStash = PlayerPrefs.GetInt("Bank");
+        }
+        collected = score.coinCount;
+        totalCoins = currentStash + collected;
+        PlayerPrefs.SetInt("Bank", totalCoins);
+    }
+
+    private void Update()
+    {
+        collected = score.coinCount;
+        bank.text = "Stash:\t" + currentStash;
+        grabbed.text = "Collected:\t" + collected;
+        total.text = "Total:\t" + (currentStash + collected);
+        Debug.Log(collected);
+    }
+
+    public void setText()
+    {
+        currentStash = PlayerPrefs.GetInt("Bank");
+        Debug.Log(collected);
+        totalCoins = currentStash + collected;
+        collected = 0;
+        PlayerPrefs.SetInt("Bank", totalCoins);
+    }
     public void RestartGame()
     {
         FindObjectOfType<GameManager>().Reset();
