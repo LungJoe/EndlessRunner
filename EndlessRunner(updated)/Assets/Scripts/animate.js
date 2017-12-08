81bd885b9b95f3329e146d88c9e4e8b2e93e0199
Jonathan Jimenez <maxte32@gmail.com>
2017-12-08 14:45:12 -0600

    Started tutorial scene. Will finish later

---
+++ EndlessRunner(updated)/Assets/Scripts/animate.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+var frames : Texture2D[];
+var framesPorSegundo = 10.0;
+
+function Start () {
+	
+}
+
+function Update () {
+        var index : int = Time.time * framesPorSegundo;
+        index = index % frames.Length;
+        GetComponent.<Renderer>().material.mainTexture = frames[index];
+}
